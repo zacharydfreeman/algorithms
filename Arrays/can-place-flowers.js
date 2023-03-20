4e0d418bf2bdc07c5e2f2374868c059586bc897d
Zachary Freeman <zdf2424@gmail.com>
2023-03-20 12:12:16 -0500

    can place flowers

--- Arrays/can-place-flowers.js
+++ Arrays/can-place-flowers.js
@@ -11,5 +11,35 @@ Output: false
 
 */
 
-// Approach: Determine max number of placements. Can only place if left and right is 0
-const canPlaceFlowers = (flowerbed, n) => {};
+// O(n) time | O(1) space
+const canPlaceFlowers = (flowerbed, n) => {
+  for (let i = 0; i < flowerbed.length; i++) {
+    if (flowerbed[i] === 0) {
+      if (
+        (flowerbed[i - 1] === 0 && flowerbed[i + 1] === 0) ||
+        (flowerbed[i - 1] === undefined && flowerbed[i + 1] === 0) ||
+        (flowerbed[i + 1] === undefined && flowerbed[i - 1] === 0) ||
+        (flowerbed[i - 1] === undefined &&
+          flowerbed[i + 1] === undefined &&
+          flowerbed[i] === 0)
+      ) {
+        flowerbed[i] = 1;
+        n--;
+      }
+    }
+  }
+
+  return n <= 0;
+};
+
+// O(n) time | O(n) space
+const canPlaceFlowers2 = (flowerbed, n) => {
+  const flowers = [0, ...flowerbed, 0];
+  for (let i = 1; i < flowers.length - 1; i++) {
+    if (flowers[i - 1] === 0 && flowers[i] === 0 && flowers[i + 1] === 0) {
+      flowers[i] = 1;
+      n -= 1;
+    }
+  }
+  return n <= 0;
+};
