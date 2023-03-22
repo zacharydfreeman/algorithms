095eff2ae43f4e8006c7c32de7662896775d77c1
Zachary Freeman <zdf2424@gmail.com>
2023-03-21 21:56:57 -0500

    min score

---
+++ Graphs/min-score.js
@@ -0,0 +1,13 @@
+/*
+You are given a positive integer n representing n cities numbered from 1 to n. You are also given a 2D array roads where roads[i] = [ai, bi, distancei] indicates that there is a bidirectional road between cities ai and bi with a distance equal to distancei. The cities graph is not necessarily connected.
+
+The score of a path between two cities is defined as the minimum distance of a road in this path.
+
+Return the minimum possible score of a path between cities 1 and n.
+
+Note:
+
+A path is a sequence of roads between two cities.
+It is allowed for a path to contain the same road multiple times, and you can visit cities 1 and n multiple times along the path.
+The test cases are generated such that there is at least one path between 1 and n.
+*/
