c0328076f8bae44f697a082fae17b467f5940864
Zachary Freeman <zdf2424@gmail.com>
2023-03-22 17:03:37 -0500

    smallest difference

--- Arrays/smallest-difference.js
+++ Arrays/smallest-difference.js
@@ -12,4 +12,28 @@ output = [28, 26]
 
 */
 
-const smallestDifference = (arrayOne, arrayTwo) => {};
+// O(nlog(n) + mlog(m)) time | O(1) space where n is length of arrayOne and m is lenght of arrayTwo
+const smallestDifference = (arrayOne, arrayTwo) => {
+  arrayOne.sort((a, b) => a - b);
+  arrayTwo.sort((a, b) => a - b);
+  let min = Infinity;
+  let output = [];
+  let i = 0;
+  let j = 0;
+
+  while (i < arrayOne.length && j < arrayTwo.length) {
+    const numOne = arrayOne[i];
+    const numTwo = arrayTwo[j];
+    const difference = Math.abs(numOne - numTwo);
+    if (difference < min) {
+      min = difference;
+      output = [numOne, numTwo];
+    }
+    if (numOne < numTwo) {
+      i++;
+    } else {
+      j++;
+    }
+  }
+  return output;
+};
