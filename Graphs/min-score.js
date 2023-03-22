f812c9f75344593bc63b4d54cffb69cdeb2429e4
Zachary Freeman <zdf2424@gmail.com>
2023-03-22 09:37:31 -0500

    min score

--- Graphs/min-score.js
+++ Graphs/min-score.js
@@ -21,7 +21,7 @@ Output: 2
 Explanation: The path from city 1 to 4 with the minimum score is: 1 -> 2 -> 1 -> 3 -> 4. The score of this path is min(2,2,4,7) = 2.
 */
 
-// O(n) time | O(n) space
+// O(v + e) time | O(v) space where v is the number of vertices and e is the number of edges
 const minScore = (n, roads) => {
   const graph = createGraph(roads);
   return dfs(1, graph, new Set());
