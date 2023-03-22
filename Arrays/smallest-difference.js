2c3b3db60951117fdbde15cbb5d76ff1ad2bf6f1
Zachary Freeman <zdf2424@gmail.com>
2023-03-22 14:07:28 -0500

    smallest difference

---
+++ Arrays/smallest-difference.js
@@ -0,0 +1,15 @@
+/* 
+
+Write a function that takes in two non-empty arrays of integers, finds the pair of numbers (one from each array) whose absolute difference is closest to zero, and returns an array containing these two numbers, with the number from the first array in the first position
+
+Note that the absolute difference of two integers is the distance between them on the real number line.
+
+You can assume that there will only be one pair of numbers with the smallest difference
+
+const arrayOne = [-1, 5, 10, 20, 28, 3]
+const arrayTwo = [26, 134, 135, 15, 17]
+output = [28, 26]
+
+*/
+
+const smallestDifference = (arrayOne, arrayTwo) => {};
