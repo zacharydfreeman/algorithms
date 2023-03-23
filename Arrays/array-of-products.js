be99c3728f55ffdf3e2fa7fe268ce36f324dadd2
Zachary Freeman <zdf2424@gmail.com>
2023-03-23 09:36:02 -0500

    array of products

---
+++ Arrays/array-of-products.js
@@ -0,0 +1,8 @@
+/* 
+Write a function that takes in a non-empty array of integers and returns an array of the same length, where each element in the output array is equal to the product of every other number in the input array
+
+array = [5, 1, 4, 2]
+output = [8, 40, 10, 20]
+*/
+
+const arrayOfProducts = (array) => {};
--- Graphs/network-connected.js
+++ Graphs/network-connected.js
@@ -17,4 +17,34 @@ Output: -1
 Explanation: There are not enough cables.
 */
 
-const makeConnected = (n, connections) => {};
+const makeConnected = (n, connections) => {
+  // convert to graph
+  const graph = createGraph(n, connections);
+  // determine how many disconnected components there are
+  let disconnectedComponents = 0;
+  for (let cable in graph) {
+    if (graph[cable].length === 0) disconnectedComponents++;
+  }
+};
+
+const createGraph = (n, connections) => {
+  const graph = {};
+  for (i = 0; i < n; i++) {
+    graph[i] = [];
+  }
+  for (let [a, b] of connections) {
+    graph[a].push(b);
+    graph[b].push(a);
+  }
+  return graph;
+};
+
+const n = 6,
+  connections = [
+    [0, 1],
+    [0, 2],
+    [0, 3],
+    [1, 2],
+    [1, 3],
+  ];
+console.log(makeConnected(n, connections));
