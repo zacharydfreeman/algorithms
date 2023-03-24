44a8031c8a6442529fff7a1264ab7c837f073944
Zachary Freeman <zdf2424@gmail.com>
2023-03-23 21:34:53 -0500

    array of products

--- Arrays/array-of-products.js
+++ Arrays/array-of-products.js
@@ -5,4 +5,21 @@ array = [5, 1, 4, 2]
 output = [8, 40, 10, 20]
 */
 
-const arrayOfProducts = (array) => {};
+// O(n) time | O(n) space
+const arrayOfProducts = (array) => {
+  const leftProducts = new Array(array.length).fill(1);
+  const rightProducts = new Array(array.length).fill(1);
+  for (let i = 1; i < leftProducts.length; i++) {
+    leftProducts[i] = array[i - 1] * leftProducts[i - 1];
+  }
+  for (let i = array.length - 2; i >= 0; i--) {
+    rightProducts[i] = array[i + 1] * rightProducts[i + 1];
+  }
+  const output = [];
+  for (let i = 0; i < array.length; i++) {
+    output[i] = leftProducts[i] * rightProducts[i];
+  }
+  return output;
+};
+
+console.log(arrayOfProducts([5, 1, 4, 2]));
