5a0b124685e6e4f68b766722711236cc9b6d28fc
zacharydfreeman <zdf2424@gmail.com>
2023-03-24 11:22:13 -0500

    heap sort

--- Sorting/sort-an-array.js
+++ Sorting/sort-an-array.js
@@ -4,7 +4,7 @@ Given an array of integers nums, sort the array in ascending order and return it
 You must solve the problem without using any built-in functions in O(nlog(n)) time complexity and with the smallest space complexity possible.
 */
 
-// Approach: Heap sort
+// Approach: Heap Sort
 // O(nlog(n)) time | O(1)
 const sortArray = (nums) => {
   // build maxheap
