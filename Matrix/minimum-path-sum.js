6c87f6675b4cbacbe42614512ea389a250b306b0
zacharydfreeman <zdf2424@gmail.com>
2023-03-26 19:26:15 -0500

    min path sum

--- Matrix/minimum-path-sum.js
+++ Matrix/minimum-path-sum.js
@@ -12,4 +12,19 @@ Output: 12
 
 */
 
-const minPathSum = (grid) => {};
+// Approach: 2-D Dynamic Programming
+// O(n * m) time | O(n * m) space
+const minPathSum = (grid, row = 0, col = 0, memo = {}) => {
+  const pos = row + ',' + col;
+  if (pos in memo) return memo[pos];
+  if (row < 0 || row >= grid.length || col < 0 || col >= grid[0].length)
+    return Infinity;
+  if (row === grid.length - 1 && col === grid[0].length - 1)
+    return grid[row][col];
+
+  const down = minPathSum(grid, row + 1, col, memo);
+  const right = minPathSum(grid, row, col + 1, memo);
+
+  memo[pos] = grid[row][col] + Math.min(down, right);
+  return memo[pos];
+};
