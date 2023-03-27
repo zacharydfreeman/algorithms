2799adeb326eeac2903cfabc6009c7d73966f18c
zacharydfreeman <zdf2424@gmail.com>
2023-03-27 12:05:43 -0500

    permutation palindrome

--- Strings/permutation-palindrome.js
+++ Strings/permutation-palindrome.js
@@ -1,22 +1,20 @@
 /**
- * Given a string, determine if any of the permutations of that string is a palindrome
- * @see: Permutations: https://stattrek.com/statistics/dictionary.aspx?definition=permutation
- * @see: Palindromes: https://examples.yourdictionary.com/palindrome-examples.html
- *
- * In terms of time complexity, see if you can solve this in O(n) / linear time.
- *
- * Example:
- * 	- permPalin('abab') => true
- * 	- permPalin('cbaba') => true
- * 	- permPalin('cbac') => false
- * 	- permPalin('a') => true
- *
- * Hint: Think about the length of the string and how that relates to the frequencies of the characters
+Given a string s, return true if a permutation of the string could form a 
+palindrome and false otherwise.
+
+Input: s = "code"
+Output: false
+
+Input: s = "aab"
+Output: true
+
+Input: s = "carerac"
+Output: true
  */
 
 // Approach: Make a char count hash map and determine how many odd counts there are
-// O(n) time | O(n) space
-const permPalin = (str) => {
+// O(n) time | O(1) space
+const canPermutePalindrome = (str) => {
   if (typeof str !== 'string') return false;
   const counts = {};
   for (let char of str) {
@@ -29,8 +27,3 @@ const permPalin = (str) => {
 
   return odd <= 1;
 };
-
-/*
- * Extension: Solve in constant space complexity.
- */
-const permPalinAdvanced = (str) => {};
