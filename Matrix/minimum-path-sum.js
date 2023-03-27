7c3fb5726f641da14c99f72516e601dea478ddc7
zacharydfreeman <zdf2424@gmail.com>
2023-03-26 19:04:50 -0500

    min path sum

---
+++ Matrix/minimum-path-sum.js
@@ -0,0 +1,15 @@
+/* 
+Given a m x n grid filled with non-negative numbers, find a path from top left to bottom right, which minimizes the sum of all numbers along its path.
+
+Note: You can only move either down or right at any point in time.
+
+Input: grid = [[1,3,1],[1,5,1],[4,2,1]]
+Output: 7
+Explanation: Because the path 1 → 3 → 1 → 1 → 1 minimizes the sum.
+
+Input: grid = [[1,2,3],[4,5,6]]
+Output: 12
+
+*/
+
+const minPathSum = (grid) => {};
