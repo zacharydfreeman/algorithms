927ea19d792a26ec6b69d7c6032b69a9a760829d
zacharydfreeman <zdf2424@gmail.com>
2023-03-27 08:50:46 -0500

    permutation palindrome

---
+++ Strings/permutation-palindrome.js
@@ -0,0 +1,22 @@
+/**
+ * Given a string, determine if any of the permutations of that string is a palindrome
+ * @see: Permutations: https://stattrek.com/statistics/dictionary.aspx?definition=permutation
+ * @see: Palindromes: https://examples.yourdictionary.com/palindrome-examples.html
+ *
+ * In terms of time complexity, see if you can solve this in O(n) / linear time.
+ *
+ * Example:
+ * 	- permPalin('abab') => true
+ * 	- permPalin('cbaba') => true
+ * 	- permPalin('cbac') => false
+ * 	- permPalin('a') => true
+ *
+ * Hint: Think about the length of the string and how that relates to the frequencies of the characters
+ */
+
+const permPalin = (str) => {};
+
+/*
+ * Extension: Solve in constant space complexity.
+ */
+const permPalinAdvanced = (str) => {};
