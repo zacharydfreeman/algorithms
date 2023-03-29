3bf95621a7cb40a6f11f2de7793664cee2e721a2
zacharydfreeman <zdf2424@gmail.com>
2023-03-28 22:56:20 -0500

    static array

---
+++ Arrays/static-array.js
@@ -0,0 +1,16 @@
+/* Create static array class */
+
+class StaticArray {
+  constructor(array) {
+    this.array = array;
+    this.capacity = this.array.length;
+  }
+
+  insertEnd() {}
+
+  insertMiddle() {}
+
+  removeMiddle() {}
+
+  printArray() {}
+}
