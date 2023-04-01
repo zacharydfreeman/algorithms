5ad53e3e6881e4a26c0772ed2b7d1bad8f565fff
zacharydfreeman <zdf2424@gmail.com>
2023-04-01 12:16:57 -0500

    reorder list

--- Linked_Lists/reorder.js
+++ Linked_Lists/reorder.js
@@ -7,6 +7,8 @@ Reorder the list to be on the following form:
 L0 → Ln → L1 → Ln - 1 → L2 → Ln - 2 → …
 You may not modify the values in the list's nodes. Only nodes themselves may be changed.
  */
+
+// O(n) time | O(1) space
 const reorderList = (head) => {
   // find the middle
   let slow = head;
