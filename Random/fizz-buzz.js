8b944db957e406064b64e045df0b3da403d29ee2
zacharydfreeman <zdf2424@gmail.com>
2023-04-08 09:45:11 -0500

    fizz buzz

--- Random/fizz-buzz.js
+++ Random/fizz-buzz.js
@@ -16,6 +16,7 @@ Input: n = 15
 Output: ["1","2","Fizz","4","Buzz","Fizz","7","8","Fizz","Buzz","11","Fizz","13","14","FizzBuzz"]
 */
 
+// O(n) time | O(1) space
 const fizzBuzz = (n) => {
   const output = [];
 
