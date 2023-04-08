fd19709316c9ff2f7f74663910d5a9f44771238e
zacharydfreeman <zdf2424@gmail.com>
2023-04-07 23:34:04 -0500

    fizz buzz

---
+++ Random/fizz-buzz.js
@@ -0,0 +1,35 @@
+/* 
+Given an integer n, return a string array answer (1-indexed) where:
+
+answer[i] == "FizzBuzz" if i is divisible by 3 and 5.
+answer[i] == "Fizz" if i is divisible by 3.
+answer[i] == "Buzz" if i is divisible by 5.
+answer[i] == i (as a string) if none of the above conditions are true.
+
+Input: n = 3
+Output: ["1","2","Fizz"]
+
+Input: n = 5
+Output: ["1","2","Fizz","4","Buzz"]
+
+Input: n = 15
+Output: ["1","2","Fizz","4","Buzz","Fizz","7","8","Fizz","Buzz","11","Fizz","13","14","FizzBuzz"]
+*/
+
+const fizzBuzz = (n) => {
+  const output = [];
+
+  for (let i = 1; i <= n; i++) {
+    if (i % 15 === 0) {
+      output.push('FizzBuzz');
+    } else if (i % 5 === 0) {
+      output.push('Buzz');
+    } else if (i % 3 === 0) {
+      output.push('Fizz');
+    } else {
+      output.push(String(i));
+    }
+  }
+
+  return output;
+};
