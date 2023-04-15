c3cb3cf25b9eda1f4b28e86faa570e9bcbe71464
zacharydfreeman <zdf2424@gmail.com>
2023-04-15 12:51:02 -0500

    max value of coins

---
+++ Dynamic_Programming/max-val-k-coins.js
@@ -0,0 +1,20 @@
+/**
+ * There are n piles of coins on a table. Each pile consists of a positive number of coins of assorted denominations.
+
+In one move, you can choose any coin on top of any pile, remove it, and add it to your wallet.
+
+Given a list piles, where piles[i] is a list of integers denoting the composition of the ith pile from top to bottom, and a positive integer k, return the maximum total value of coins you can have in your wallet if you choose exactly k coins optimally.
+
+Input: piles = [[1,100,3],[7,8,9]], k = 2
+Output: 101
+Explanation:
+The above diagram shows the different ways we can choose k coins.
+The maximum total we can obtain is 101.
+
+Input: piles = [[100],[100],[100],[100],[100],[100],[1,1,1,1,1,1,700]], k = 7
+Output: 706
+Explanation:
+The maximum total can be obtained if we choose all coins from the last pile.
+ */
+
+const maxValueOfCoins = (piles, k) => {};
