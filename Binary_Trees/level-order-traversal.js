08daf646879a40145729924d5f9b0ee371d11ea2
zacharydfreeman <zdf2424@gmail.com>
2023-04-24 20:21:24 -0500

    level order traversal

--- Binary_Trees/level-order-traversal.js
+++ Binary_Trees/level-order-traversal.js
@@ -2,10 +2,37 @@
 Given the root of a binary tree, return the level order traversal of its nodes' values. (i.e., from left to right, level by level).
 */
 
+// Approach: BFS using current level instead of a queue
+// O(n) time | O(n) space
+const levelOrder = (root) => {
+  // if root is null return empty array
+  if (!root) return [];
+  // declare output array and initialize current level
+  const output = [];
+  let currentLevel = [root];
+  while (currentLevel.length) {
+    // put the values into output from current level
+    const length = currentLevel.length;
+    const newLevel = [];
+    const vals = [];
+    for (let i = 0; i < length; i++) {
+      const current = currentLevel[i];
+      vals.push(current.val);
+      if (current.left) newLevel.push(current.left);
+      if (current.right) newLevel.push(current.right);
+    }
+    // push vals into output
+    output.push(vals);
+    // update current level
+    currentLevel = newLevel;
+  }
+  return output;
+};
+
 // Approach: BFS
 // O(n) time | O(n) space where n is the number of nodes and h is the height of the tree.
 // We are ignoring the additional time from shift() in javascript
-const levelOrder = (root) => {
+const levelOrder2 = (root) => {
   // if root is null return empty array
   if (!root) return [];
   // declare output array and queue initialzed with root node and level of 0
