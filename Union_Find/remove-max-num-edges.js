705c22dfd9b40c37413e225da12111db0d6c8174
zacharydfreeman <zdf2424@gmail.com>
2023-04-30 12:12:31 -0500

    remove max number of edges

--- Union_Find/remove-max-num-edges.js
+++ Union_Find/remove-max-num-edges.js
@@ -96,15 +96,3 @@ class UnionFind {
     return this.unconnectedComponents <= 1;
   }
 }
-
-const n = 4,
-  edges = [
-    [3, 1, 2],
-    [3, 2, 3],
-    [1, 1, 3],
-    [1, 2, 4],
-    [1, 1, 2],
-    [2, 3, 4],
-  ];
-
-console.log(maxNumEdgesToRemove(n, edges));
