466e468702877f37eb615c2f4fa2236cf908b417
zacharydfreeman <zdf2424@gmail.com>
2023-05-12 09:58:23 -0500

    longest common subsequence

--- Dynamic_Programming/longest-common-subsequence.js
+++ Dynamic_Programming/longest-common-subsequence.js
@@ -19,10 +19,28 @@ Output: 0
 Explanation: There is no such common subsequence, so the result is 0.
  */
 
-// Approach: Recursive or Tabulation DP
+// Approach: 2D DP with Tabulation Optimized for Space
+// O(n * m) time | O(n) space
+const longestCommonSubsequence = (text1, text2) => {
+  let previousRow = new Array(text1.length + 1).fill(0);
+  for (let row = 1; row <= text2.length; row++) {
+    const currentRow = new Array(text1.length + 1).fill(0);
+    for (let col = 1; col <= text1.length; col++) {
+      if (text1[col - 1] === text2[row - 1]) {
+        currentRow[col] = 1 + previousRow[col - 1];
+      } else {
+        currentRow[col] = Math.max(currentRow[col - 1], previousRow[col]);
+      }
+    }
+    previousRow = currentRow;
+  }
+  return previousRow[text1.length];
+};
+
+// Approach: 2D DP with Tabulation
 // Build a 2d matrix that essentially solves the problem but with smaller strings
 // O(n * m) time | O(n * m) space
-const longestCommonSubsequence = (text1, text2) => {
+const longestCommonSubsequence2 = (text1, text2) => {
   // initialize the 2d array
   const dp = [];
   for (let row = 0; row <= text1.length; row++) {
@@ -38,11 +56,7 @@ const longestCommonSubsequence = (text1, text2) => {
       if (text1[row - 1] === text2[col - 1]) {
         dp[row][col] = 1 + dp[row - 1][col - 1];
       } else {
-        dp[row][col] = Math.max(
-          dp[row][col - 1],
-          dp[row - 1][col],
-          dp[row - 1][col - 1]
-        );
+        dp[row][col] = Math.max(dp[row][col - 1], dp[row - 1][col]);
       }
     }
   }
