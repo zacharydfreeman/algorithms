4116780df3f95829ccee87ef8682372d7d8de212
zacharydfreeman <zdf2424@gmail.com>
2023-05-13 11:55:42 -0500

    count ways to build good string

---
+++ Dynamic_Programming/count-ways-good-strings.js
@@ -0,0 +1,62 @@
+/*
+Given the integers zero, one, low, and high, we can construct a string by starting with an empty string, and then at each step perform either of the following:
+
+Append the character '0' zero times.
+Append the character '1' one times.
+This can be performed any number of times.
+
+A good string is a string constructed by the above process having a length between low and high (inclusive).
+
+Return the number of different good strings that can be constructed satisfying these properties. Since the answer can be large, return it modulo 109 + 7.
+
+Input: low = 3, high = 3, zero = 1, one = 1
+Output: 8
+Explanation: 
+One possible valid good string is "011". 
+It can be constructed as follows: "" -> "0" -> "01" -> "011". 
+All binary strings from "000" to "111" are good strings in this example.
+
+Input: low = 2, high = 3, zero = 1, one = 2
+Output: 5
+Explanation: The good strings are "00", "11", "000", "110", and "011".
+
+ */
+
+// Approach: DP with Tabulation
+// O(high) time | O(high) space
+const countGoodStrings = (low, high, zero, one) => {
+  const dp = new Array(high + 1).fill(0);
+  dp[0] = 1;
+  for (let i = 1; i < dp.length; i++) {
+    if (i - zero >= 0) dp[i] += dp[i - zero] % MOD;
+    if (i - one >= 0) dp[i] += dp[i - one] % MOD;
+  }
+  let total = 0;
+  for (let i = low; i <= high; i++) {
+    total += dp[i];
+  }
+  return total % MOD;
+};
+
+// Approach: DP with Recursion
+// O(high) time | O(high) space
+const MOD = Math.pow(10, 9) + 7;
+
+const countGoodStrings2 = (low, high, zero, one, length = 0, memo = {}) => {
+  if (length in memo) return memo[length];
+  if (length > high) return 0;
+
+  let count = length >= low ? 1 : 0;
+  count +=
+    countGoodStrings(low, high, zero, one, length + zero, memo) +
+    countGoodStrings(low, high, zero, one, length + one, memo);
+  memo[length] = count;
+  return memo[length] % MOD;
+};
+
+const low = 2,
+  high = 3,
+  zero = 1,
+  one = 2;
+
+console.log(countGoodStrings(low, high, zero, one));
