ec556d2a70328b18176a4cdcb8a5257e3432c813
zacharydfreeman <zdf2424@gmail.com>
2023-06-03 02:33:55 -0400

    shuffle an array

---
+++ Arrays/shuffle-an-array.js
@@ -0,0 +1,28 @@
+/* 
+Given the array nums consisting of 2n elements in the form [x1,x2,...,xn,y1,y2,...,yn].
+
+Return the array in the form [x1,y1,x2,y2,...,xn,yn].
+
+Input: nums = [2,5,1,3,4,7], n = 3
+Output: [2,3,5,4,1,7] 
+Explanation: Since x1=2, x2=5, x3=1, y1=3, y2=4, y3=7 then the answer is [2,3,5,4,1,7].
+
+Input: nums = [1,2,3,4,4,3,2,1], n = 4
+Output: [1,4,2,3,3,2,4,1]
+
+Input: nums = [1,1,2,2], n = 2
+Output: [1,2,1,2]
+
+
+*/
+
+// O(n) time | O(n) space
+const shuffle = (nums, n) => {
+  const output = [];
+  let l = 0;
+  let r = n;
+  while (r < nums.length) {
+    output.push(nums[l++], nums[r++]);
+  }
+  return output;
+};
