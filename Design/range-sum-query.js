1449254de7889f1bfeceff6b28f4282411153f0b
zacharydfreeman <zdf2424@gmail.com>
2023-06-09 14:40:26 -0400

    range sum query

---
+++ Design/range-sum-query.js
@@ -0,0 +1,38 @@
+/* 
+Given an integer array nums, handle multiple queries of the following type:
+
+Calculate the sum of the elements of nums between indices left and right inclusive where left <= right.
+Implement the NumArray class:
+
+NumArray(int[] nums) Initializes the object with the integer array nums.
+int sumRange(int left, int right) Returns the sum of the elements of nums between indices left and right inclusive (i.e. nums[left] + nums[left + 1] + ... + nums[right]).
+
+Input
+["NumArray", "sumRange", "sumRange", "sumRange"]
+[[[-2, 0, 3, -5, 2, -1]], [0, 2], [2, 5], [0, 5]]
+Output
+[null, 1, -1, -3]
+
+Explanation
+NumArray numArray = new NumArray([-2, 0, 3, -5, 2, -1]);
+numArray.sumRange(0, 2); // return (-2) + 0 + 3 = 1
+numArray.sumRange(2, 5); // return 3 + (-5) + 2 + (-1) = -1
+numArray.sumRange(0, 5); // return (-2) + 0 + 3 + (-5) + 2 + (-1) = -3
+*/
+
+class NumArray {
+  constructor(nums) {
+    this.prefixSums = [];
+    let total = 0;
+    for (let num of nums) {
+      total += num;
+      this.prefixSums.push(total);
+    }
+  }
+
+  sumRange(left, right) {
+    let leftPrefix = left > 0 ? this.prefixSums[left - 1] : 0;
+    let rightPrefix = this.prefixSums[right];
+    return rightPrefix - leftPrefix;
+  }
+}
