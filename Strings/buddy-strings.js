17ba52264b486c0fa5ca1de67493c1d51a78e8ac
zacharydfreeman <zdf2424@gmail.com>
2023-07-02 22:50:49 -0500

    buddy strings

--- Strings/buddy-strings.js
+++ Strings/buddy-strings.js
@@ -16,6 +16,45 @@ Explanation: The only letters you can swap are s[0] = 'a' and s[1] = 'b', which 
 Input: s = "aa", goal = "aa"
 Output: true
 Explanation: You can swap s[0] = 'a' and s[1] = 'a' to get "aa", which is equal to goal.
+
+abcde
+abcde
 */
 
-const buddyStrings = (s, goal) => {};
+// O(n) time | O(1) space
+const buddyStrings = (s, goal) => {
+  // if strings arent equal length return false
+  if (s.length !== goal.length) return false;
+  // check freq of chars and make sure each char has same frequency
+  const [sMap, gMap] = [getFreqMap(s), getFreqMap(goal)];
+  if (!checkMaps(sMap, gMap)) return false;
+  let count = 0; // number of mismatched letters
+  for (let i = 0; i < s.length; i++) {
+    if (s[i] !== goal[i]) count++;
+  }
+  // if count is 2 return true
+  if (count === 2) return true;
+  // if count is 0
+  if (count === 0) {
+    if (Object.keys(sMap).length === s.length) return false;
+    return true;
+  }
+
+  return false;
+};
+
+const getFreqMap = (s) => {
+  const map = {};
+  for (let char of s) {
+    map[char] = map[char] + 1 || 1;
+  }
+  return map;
+};
+
+const checkMaps = (m1, m2) => {
+  for (let char in m1) {
+    if (!(char in m2)) return false;
+    if (m1[char] !== m2[char]) return false;
+  }
+  return true;
+};
