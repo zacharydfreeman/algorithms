6c00699ed66a9f3dee48dbd8a6823c555f3cbd96
zacharydfreeman <zdf2424@gmail.com>
2023-07-04 15:47:17 -0500

    binary search tree iterator

---
+++ Binary_Trees/binary-search-iterator.js
@@ -0,0 +1,50 @@
+/* 
+Implement the BSTIterator class that represents an iterator over the in-order traversal of a binary search tree (BST):
+
+BSTIterator(TreeNode root) Initializes an object of the BSTIterator class. The root of the BST is given as part of the constructor. The pointer should be initialized to a non-existent number smaller than any element in the BST.
+boolean hasNext() Returns true if there exists a number in the traversal to the right of the pointer, otherwise returns false.
+int next() Moves the pointer to the right, then returns the number at the pointer.
+Notice that by initializing the pointer to a non-existent smallest number, the first call to next() will return the smallest element in the BST.
+
+You may assume that next() calls will always be valid. That is, there will be at least a next number in the in-order traversal when next() is called.
+
+["BSTIterator", "next", "next", "hasNext", "next", "hasNext", "next", "hasNext", "next", "hasNext"]
+[[[7, 3, 15, null, null, 9, 20]], [], [], [], [], [], [], [], [], []]
+Output
+[null, 3, 7, true, 9, true, 15, true, 20, false]
+
+Explanation
+BSTIterator bSTIterator = new BSTIterator([7, 3, 15, null, null, 9, 20]);
+bSTIterator.next();    // return 3
+bSTIterator.next();    // return 7
+bSTIterator.hasNext(); // return True
+bSTIterator.next();    // return 9
+bSTIterator.hasNext(); // return True
+bSTIterator.next();    // return 15
+bSTIterator.hasNext(); // return True
+bSTIterator.next();    // return 20
+bSTIterator.hasNext(); // return False
+
+*/
+
+class BSTIterator {
+  constructor(root) {
+    this.inorder = [];
+    this.index = 0;
+    const dfs = (node) => {
+      if (!node) return;
+      dfs(node.left);
+      this.inorder.push(node.val);
+      dfs(node.right);
+    };
+    dfs(root);
+  }
+
+  next() {
+    return this.inorder[this.index++];
+  }
+
+  hasNext() {
+    return this.index !== this.inorder.length;
+  }
+}
