11f8a2c55502a51208136afb11d069bdc21d5b00
zacharydfreeman <zdf2424@gmail.com>
2023-09-06 18:37:41 -0500

    med school matching

---
+++ Miscellaneous/medschool-matching.js
@@ -0,0 +1,25 @@
+/*
+Create algorithm that matches med school students for residency
+*/
+
+class Student {
+  constructor(name) {
+    this.name = name;
+    this.ranks = {};
+  }
+}
+
+class School {
+  constructor(name, capacity) {
+    this.name = name;
+    this.capacity = capacity;
+    this.spots = [];
+    1;
+    this.ranks = {};
+  }
+}
+
+const match = (students, schools) => {};
+
+const toliver = new Student('toliver');
+const duke = new School('Duke', 5);
